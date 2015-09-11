ae144e08bbb143f96deb60893e36e43c95b455b9
499478785 <499478785@qq.com>
2015-09-11 09:39:58 +0800

    thinkphp  test

---
+++ Application/Common/Conf/db.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/9/11
+ * Time: 9:21
+ */
+return [
+    /* 数据库设置 */
+    'DB_TYPE'         => 'mysql',   // 数据库类型
+    'DB_HOST'         => '',   // 服务器地址
+    'DB_NAME'         => '',   // 数据库名
+    'DB_USER'         => '',   // 用户名
+    'DB_PWD'          => '',   // 密码
+    'DB_PORT'         => '',   // 端口
+    'DB_PREFIX'       => '',   // 数据库表前缀
+    'DB_DEBUG'        => true, // 数据库调试模式 开启后可以记录SQL日志
+    'DB_FIELDS_CACHE' => true, // 启用字段缓存
+];
---
+++ Application/Home/Common/function.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/9/11
+ * Time: 9:15
+ */
