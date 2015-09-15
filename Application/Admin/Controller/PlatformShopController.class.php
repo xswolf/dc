5af9a3c527961ec9120dbce16a0b1c6fe76e2aa8
499478785 <499478785@qq.com>
2015-09-15 13:18:00 +0800

    thinkphp  test

---
+++ Application/Admin/Controller/PlatformShopController.class.php
@@ -0,0 +1,16 @@
+<?php
+namespace Admin\Controller;
+use Common\Controller\VerifyController;
+
+class PlatformShopController extends VerifyController
+
+{
+    public function index()
+    {
+
+    }
+
+    public function add(){
+
+    }
+}
---
+++ Application/Admin/Model/PlatformShopModel.class.php
@@ -0,0 +1,8 @@
+<?php
+namespace Admin\Model;
+use Common\Model\BaseModel;
+
+class PlatformShopModel extends BaseModel {
+
+    protected $_table = "platform_shop";
+}
