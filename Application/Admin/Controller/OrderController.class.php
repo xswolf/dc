61fed345e2c21d0ae700abfbc043edaf6a56dfba
499478785@qq.com <dc123456>
2015-09-23 10:47:52 +0800

    当前订单

--- Application/Admin/Controller/OrderController.class.php
+++ Application/Admin/Controller/OrderController.class.php
@@ -1,10 +1,34 @@
 <?php
 namespace Admin\Controller;
-use Common\Controller\BaseController;
+use Common\Controller\VerifyController;
 
-class OrderController extends BaseController {
+use Admin\Model\OrderModel;
+class OrderController extends VerifyController {
+    /**
+     * @var int 平台ID
+     */
+    protected $platform_id;
+
+    /**
+     * @var int 店铺ID
+     */
+    protected $shop_id;
+
+    public function _before_order() {
+        $this->shop_id = $this->user->getShopId();
+    }
 
     public function index(){
         $this->display();
     }
+
+    /**
+     * 商家管理平台 -> 当前订单
+     */
+    public function order() {
+        $data = OrderModel::instance()->orderList($this->shop_id);
+        $this->assign('data',$data);
+        $this->display();
+    }
+
 }
--- Application/Admin/Model/OrderModel.class.php
+++ Application/Admin/Model/OrderModel.class.php
@@ -1,6 +1,19 @@
 <?php
 namespace Admin\Model;
-use Think\Model;
-class OrderModel extends Model {
+use Common\Model\BaseModel;
 
+class OrderModel extends BaseModel {
+    /**
+     * @var string 订单表
+     */
+    protected $_table = "order";
+
+    /**
+     * 订单列表
+     * @param int $shop_id
+     * @return array
+     */
+    public function orderList($shop_id) {
+
+    }
 }
