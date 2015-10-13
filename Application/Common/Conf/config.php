284d0d60c6e367f2c33e85ab6add49233e4d8abe
guoyong <a515303348@qq.com>
2015-10-13 20:11:27 +0800

    新增模块WX

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,7 +6,7 @@ return array(
     'SESSION_AUTO_START' => true,   //是否开启session
     'LOAD_EXT_CONFIG'    => 'db',      //加载扩展配置文件
     'MODULE_DENY_LIST'   => ['Common', 'Runtime'],
-    'MODULE_ALLOW_LIST'  => ['Home', 'Admin', 'Site'],
+    'MODULE_ALLOW_LIST'  => ['Home', 'Admin', 'Site','Wx'],
     'SHOW_PAGE_TRACE'=>1,
     'DEFAULT_MODULE'     => 'Site',
 );
