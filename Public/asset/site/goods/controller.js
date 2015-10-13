5b45ebf6f48e2d329dc4918d01b5de2c2b2faa0a
499478785@qq.com <dc123456>
2015-10-13 09:14:10 +0800

    test

---
+++ Public/asset/site/goods/controller.js
@@ -0,0 +1,13 @@
+/**
+ * goods controller
+ * User: hgh
+ * Date: 2015/10/12
+ * Time: 14:30
+ */
+define(['./module.js'], function(module) {
+    module.controller('goods.Controller', ['$scope', function($scope) {
+        $scope.numbers = 600;
+        $scope.price = 905;
+
+    }]);
+});
