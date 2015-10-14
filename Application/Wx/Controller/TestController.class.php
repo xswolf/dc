8a0f5454e0b6d86bcc97e215511e00cfd10546a3
guoyong <a515303348@qq.com>
2015-10-14 09:35:13 +0800

    支付测试

--- Application/Wx/Controller/TestController.class.php
+++ Application/Wx/Controller/TestController.class.php
@@ -12,6 +12,7 @@ class TestController extends InitiativeController{
 //         ISendMessageController::instance(['tousername'=>"aaa"])->scanTableQrcode();
         $result = PayEvent::instance()->JsApiPay(2 , 1 , '高圆圆' , '嘎嘎嘎' ,'ggg') ;
         if($result['status']==1){
+            dump($result['message']);
             $this->assign('json', $result['message']);
             $this->display();
         }
