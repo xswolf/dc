af202342251e303800e73e9c456c9b9789574b89
guoyong <a515303348@qq.com>
2015-10-15 14:25:27 +0800

    修改结构

---
+++ Application/Wx/Event/ArticlesEvent.class.php
@@ -0,0 +1,45 @@
+<?php
+namespace Application\Wx\Event;
+
+use Common\Controller\BaseController;
+use LaneWeChat\Core\Media;
+use LaneWeChat\Core\AdvancedBroadcast;
+
+class ArticlesEvent extends BaseController{
+    
+    //上传图片
+    public function uploadImg(){
+        //'ybFlQ9kmPTgiCWZT8Mq_1YnKtBziwpbaVDaftpJckPtFzlwROlgXpKCd9Dbl_JRs'
+        if(!empty($_FILES)){
+            $info = reset( upload() );
+    
+            $data = Media::upload(realpath( $info['file_save_path'] ), 'image');
+            file_put_contents('./wx-test/uploadimg.txt', json_encode($data));
+            dump($data);
+        }
+        //        dump(  Media::upload(realpath('./Public/Uploads/2015-09-23/560218d10525f.jpg'),'image' ));
+    }
+    
+    public function download(){
+        file_put_contents('./wx-test/down.jpg', Media::download('Hva-LYOON6lXC_MiS8H9OGDSisWZXyAWn3deI0SM7iEL0Uy2mdFHT4yaTabTFiq4'));
+    }
+    
+    
+    //新增图文消息
+    public function uploadNews(){
+        //'pk7-ztaDrb1tKm6mPwKtxJO50386VvsJ6qgDw6isuUypA94v9xj3RdXYUMuqkI4b'
+        $articles = [
+            [
+                'title'         =>  '请戳图',
+                'thumb_media_id'=>  'ybFlQ9kmPTgiCWZT8Mq_1YnKtBziwpbaVDaftpJckPtFzlwROlgXpKCd9Dbl_JRs',
+                'author'        =>  'gy',
+                'digest'        =>  '哈哈哈哈哈哈哈',
+                'show_cover_pic'=>  1,
+                'content'       =>  '',
+                'content_source_url'=>'www.baidu.com'
+            ]
+        ];
+        $info = AdvancedBroadcast::uploadNews($articles);
+        dump($info);
+    }
+}
