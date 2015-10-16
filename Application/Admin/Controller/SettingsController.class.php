2bc478e9c884131a85b7e8e7b834d810cd3bbe92
499478785 <499478785@qq.com>
2015-10-16 15:24:45 +0800

    c

--- Application/Admin/Controller/SettingsController.class.php
+++ Application/Admin/Controller/SettingsController.class.php
@@ -5,8 +5,6 @@ use Admin\Model\GoodsModel;
 use Admin\Model\GoodsTypeModel;
 use Admin\Model\ShopTableModel;
 use Common\Controller\VerifyController;
-use Wx\Event\QrcodeEvent;
-
 class SettingsController extends VerifyController {
 
     public function save(){
@@ -71,19 +69,8 @@ class SettingsController extends VerifyController {
 
                 ShopTableModel::instance()->edit($_POST);
             }else{ // 添加
-
-                $qrCode = new QrcodeEvent();
-                $data = $qrCode->create($this->user->getShopId());
-
-                if ($data){
-                    $_POST['qrcode_id'] = $data['qrcode_id'];
-                    $_POST['url'] = $data['url'];
-                    $_POST['shop_id'] = $this->user->getShopId();
-                    ShopTableModel::instance()->insert($_POST);
-                }else{
-
-                }
-
+                $_POST['shop_id'] = $this->user->getShopId();
+                ShopTableModel::instance()->insert($_POST);
             }
             $this->_success('添加成功' , U('table'));
 
