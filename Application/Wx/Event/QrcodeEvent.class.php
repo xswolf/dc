5f4127e300a67de7549556b893f613bf030e7ea2
guoyong <a515303348@qq.com>
2015-10-16 11:10:44 +0800

    二维码

--- Application/Wx/Event/QrcodeEvent.class.php
+++ Application/Wx/Event/QrcodeEvent.class.php
@@ -10,15 +10,16 @@ class Qrcode extends BaseController{
 
     /**
      * 创建二维码
+     * @param number $shop_id
+     * @param string $group     分组
      * @param number $type      1临时|2永久
      * @param number $expire    临时过期时间最大604800
-     * @param string $group     分组
      * @param string $sene_str  场景值 永久二维码才有
      * @return array|boolean
      */
-    public function create( $type = 1 , $expire=604800 , $group="table" , $sene_str=''){
+    public function create( $shop_id , $group="table" , $type = 1 , $expire=604800 , $sene_str=''){
         $sene_id = QrcodeModel::instance()->getSceneId($type);
-        $data = Popularize::createTicket($type, 604800, $sene_id);
+        $data = Popularize::createTicket($type, $expire, $sene_id);
         if(empty($data['errcode'])){
             $qrcode_path = "./Public/Wx/Qrcode/".date('Ymd').'/';
             if( !file_exists($qrcode_path) ){
@@ -32,7 +33,7 @@ class Qrcode extends BaseController{
                 'type'       =>  $type,
                 'expire'     =>  $expire,
                 'url'        =>  ltrim( $qrcode_url , '.' ),
-                'shop_id'    =>  0,
+                'shop_id'    =>  $shop_id,
                 'groups'      => $group,
                 'status'     =>  1,
                 'created_at' =>  NOW_TIME,
