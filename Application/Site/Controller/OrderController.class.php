b57758949914a5334741e96db005bedb262ea741
499478785@qq.com <dc123456>
2015-10-17 14:09:40 +0800

    前端

--- Application/Site/Controller/OrderController.class.php
+++ Application/Site/Controller/OrderController.class.php
@@ -115,7 +115,7 @@ class OrderController extends BaseController {
 			if(ctype_digit($order['mid'])) {
 				$wx_usr = OrderModel::instance()->getWxUser($order['mid']);
 				if(!empty($wx_usr) && is_array($wx_usr)) {
-					$wx_pay = PayEvent::instance()->JsApiPay($wx_usr['id'],$order['sn'],$order['price']*100);
+					$wx_pay = PayEvent::instance()->JsApiPay($wx_usr['id'],$order['sn'],$order['price']*100,'商品支付');
 					var_export($wx_pay);
 					exit;
 					if($wx_pay['status'] == 1) {
