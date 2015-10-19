e0567df6cfd0cfc5eeda1084facd25b1a680c06c
guoyong <a515303348@qq.com>
2015-10-19 18:47:04 +0800

    微信菜单

--- Application/Wx/Controller/MenuController.class.php
+++ Application/Wx/Controller/MenuController.class.php
@@ -2,6 +2,7 @@
 namespace Wx\Controller;
 
 use Common\Controller\BaseController;
+use Wx\Model\MenuModel;
 
 /**
  * 微信自定义菜单管理
@@ -22,16 +23,23 @@ class MenuController extends BaseController{
             $data = [
                 'name' => $_POST['name'],
                 'type' => $_POST['type'],
+                'pid'  => empty($_POST['pid']) ? 0 : intval($_POST['pid']),
             ];
             if($data['type']==2){
                 empty($_POST['url']) && $this->ajaxError('请填写url');
                 $data['value'] = $_POST['url'];
-            }
-            if($data['type']==3){
+            }elseif($data['type']==3){
                 empty($_POST['material']) && $this->ajaxError('请选择素材');
                 $data['value'] = $_POST['material'];
             }
+            $num = MenuModel::instance()->getMenuSum($data['pid']);
+            if($data['pid'] && $num >= 5){
+                $this->ajaxError("二级菜单已达到数量上限");
+            }elseif ($data['pid']==0 && $num >= 3){
+                $this->ajaxError('一级菜单已达数量上限');
+            }
             
+            M("wx_menu");
         }
     }
     
