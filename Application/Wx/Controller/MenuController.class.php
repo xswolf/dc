fcde33a27c809d8014511060c63b0f5dff88abdd
guoyong <a515303348@qq.com>
2015-10-19 10:27:08 +0800

    微信菜单

--- Application/Wx/Controller/MenuController.class.php
+++ Application/Wx/Controller/MenuController.class.php
@@ -17,5 +17,23 @@ class MenuController extends BaseController{
     
     public function add(){
         
+        if(IS_AJAX){
+            empty($_POST['name']) && $this->ajaxError("请填写菜单名");
+            empty($_POST['type']) && !intval($_POST['type']) && $this->ajaxError("菜单类型错误");
+            $data = [
+                'name' => $_POST['name'],
+                'type' => $_POST['type'],
+            ];
+            if($data['type']==2){
+                empty($_POST['url']) && $this->ajaxError('请填写url');
+                $data['value'] = $_POST['url'];
+            }
+            if($data['type']==3){
+                empty($_POST['material']) && $this->ajaxError('请选择素材');
+                $data['value'] = $_POST['material'];
+            }
+            
+        }
     }
+    
 }
