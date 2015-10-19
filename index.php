b51659647135c6edacb3abb33ba0ba80e719115f
499478785 <499478785@qq.com>
2015-10-19 17:06:29 +0800

    c

--- index.php
+++ index.php
@@ -14,8 +14,11 @@
 // 检测PHP环境
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
+define('BUILD_LITE_FILE',true);
+
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
-define('APP_DEBUG',true);
+define('APP_DEBUG',false);
+
 
 // 定义应用目录
 define('APP_PATH','./Application/');
@@ -26,3 +29,5 @@ require './LaneWeChat/lanewechat.php';
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
 // 亲^_^ 后面不需要任何代码了 就是如此简单
+
+//require APP_PATH.'/Runtime/lite.php';
