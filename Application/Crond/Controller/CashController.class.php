4ecba90651511360fdad087dc149537b852161e9
499478785 <499478785@qq.com>
2015-10-22 18:11:10 +0800

    c

--- Application/Crond/Controller/CashController.class.php
+++ Application/Crond/Controller/CashController.class.php
@@ -14,6 +14,7 @@ class CashController extends \Think\Controller{
         $sql = "INSERT INTO w_cash(shop_id,create_date,cash_money,orderCount,STATUS)
                 SELECT shop_id , FROM_UNIXTIME(created_at,'%Y%m%d') created_date,  SUM(price) , COUNT(*) , 0 FROM  w_order WHERE STATUS in(2,3) AND FROM_UNIXTIME(created_at,'%Y%m%d') = {$date} GROUP BY shop_id ,created_date";
 
+
         return M('')->query($sql);
     }
 
